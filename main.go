e80cec70a727672988dd84df6b2f6bf77cc91a61
Shikhar Kannoje <shikhar.kannoje@agiliad.local>
2020-11-02 11:27:34 +0530

    package change

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/ShikharKannoje/RabbitMQProducer/formater"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -75,39 +76,17 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 
-	JSON(w, http.StatusOK, "Sender UP and running")
+	formater.JSON(w, http.StatusOK, "Sender UP and running")
 
 }
 
 func app(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		ERROR(w, http.StatusUnprocessableEntity, err)
+		formater.ERROR(w, http.StatusUnprocessableEntity, err)
 		return
 	}
 
 	fmt.Println(body)
 	initialize(body)
-}
-
-//JSON formatter
-func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
-	w.WriteHeader(statusCode)
-	err := json.NewEncoder(w).Encode(data)
-	if err != nil {
-		fmt.Fprintf(w, "%s", err.Error())
-	}
-}
-
-//ERROR function
-func ERROR(w http.ResponseWriter, statusCode int, err error) {
-	if err != nil {
-		JSON(w, statusCode, struct {
-			Error string `json:"error"`
-		}{
-			Error: err.Error(),
-		})
-		return
-	}
-	JSON(w, http.StatusBadRequest, nil)
 }
